d76f86c2b00e466fc8e8fd86a2becdf8f5a8d987
ch6vip <ch6vip@gmail.com>
2025-10-09 16:09:20 +0800

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="purcarte-blur p-2 border-b border-(--accent-a4) inset-shadow-sm inset-shadow-(color:--accent-a4) sticky bottom-0 flex items-center justify-center z-10">
       <p className="flex justify-center text-sm text-secondary-foreground theme-text-shadow whitespace-pre">
         {/* "Powered by Komari Monitor" 部分已被删除 */}
-        Theme by{" "}
+        Monitored by{" "}
         <a
           href="https://jk.0z0.tech/status/all"
           target="_blank"
